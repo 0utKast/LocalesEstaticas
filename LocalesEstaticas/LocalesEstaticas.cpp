f8b2f64f694f424f1a80650cf859867f6fc53869
Jesus Conde <educaleft@gmail.com>
2021-12-13 13:42:30 +0000

    Añadida versión de incrementaEImprime con variable local estática.

--- LocalesEstaticas/LocalesEstaticas.cpp
+++ LocalesEstaticas/LocalesEstaticas.cpp
@@ -2,6 +2,40 @@
 
 void incrementaEImprime()
 {
+    static int s_valor{ 1 }; // duración estática por palabra clave.  Este inicializador se ejecuta sólo una vez.
+    ++s_valor;
+    std::cout << s_valor << '\n';
+} // s_valor no se destruye aquí, pero se convierte en inaccesible porque está fuera de alcance.
+
+int main()
+{
+    incrementaEImprime();
+    incrementaEImprime();
+    incrementaEImprime();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <iostream>
+
+void incrementaEImprime()
+{
     int valor{ 1 }; // duración automática predeterminada
     ++valor;
     std::cout << valor << '\n';
@@ -14,4 +48,4 @@ int main()
     incrementaEImprime();
 
     return 0;
-}
+}*/
