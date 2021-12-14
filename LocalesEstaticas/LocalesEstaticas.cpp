f8fe4e862f29a07d0d76d599ba1edfe3df60cfd2
Jesus Conde <educaleft@gmail.com>
2021-12-14 13:57:28 +0000

    Código para VT 45 del Curso de C++ Básico completo

--- LocalesEstaticas/LocalesEstaticas.cpp
+++ LocalesEstaticas/LocalesEstaticas.cpp
@@ -1,5 +1,134 @@
 #include <iostream>
 
+int generateID()
+{
+    static int s_itemID{ 0 };
+     // hace copia de s_itemID, incrementa el real s_itemID, luego devuelve el valor de la copia
+    std::cout << s_itemID << '\n';
+    return s_itemID++;
+}
+
+int main()
+{   
+    generateID();
+    generateID();
+    generateID();
+    generateID();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <iostream>
+
+void incrementaEImprime()
+{
+    static int s_valor{ 1 }; // duración estática por palabra clave.  
+                             //Este inicializador se ejecuta sólo una vez.
+    ++s_valor;
+    std::cout << s_valor << '\n';
+} // s_valor no se destruye aquí, pero se convierte en inaccesible porque está fuera de alcance.
+
+
+int main()
+{
+   
+    incrementaEImprime();
+    incrementaEImprime();
+    incrementaEImprime(); 
+
+
+    return 0;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <iostream>
+
+void incrementaEImprime()
+{
+    int valor{ 1 }; // duración automática predeterminada
+    ++valor;
+    std::cout << valor << '\n';
+} // valor se destruye aquí
+
+int main()
+{
+    incrementaEImprime();
+    incrementaEImprime();
+    incrementaEImprime();
+
+    return 0;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <iostream>
+
 int obtenerInteger()
 {
     static bool s_esPrimeraLlamada{ true };
@@ -27,7 +156,7 @@ int main()
     std::cout << a << " + " << b << " = " << (a + b) << '\n';
 
     return 0;
-}
+}*/
 
 
 
@@ -83,20 +212,3 @@ int main()
 
 
 
-/*#include <iostream>
-
-void incrementaEImprime()
-{
-    int valor{ 1 }; // duración automática predeterminada
-    ++valor;
-    std::cout << valor << '\n';
-} // valor se destruye aquí
-
-int main()
-{
-    incrementaEImprime();
-    incrementaEImprime();
-    incrementaEImprime();
-
-    return 0;
-}*/
